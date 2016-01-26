84eadac2ad340c948d60ca3127b6b423360864b7
Yusuke Wada <yusuke@kamawada.com>
2016-01-27 00:35:39 +0900

    handle static files.

--- cli.go
+++ cli.go
@@ -9,5 +9,5 @@ type CLI struct {
 
 func (cli *CLI) Run() {
 	server := Server{port: 3000}
-	server.serve()
+	server.Serve()
 }
--- server.go
+++ server.go
@@ -2,26 +2,46 @@ package revealgo
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/elazarl/go-bindata-assetfs"
+	"strconv"
+	"strings"
 )
 
 type Server struct {
 	port int
 }
 
-func (server *Server) serve() {
-	http.Handle("/revealjs/", http.StripPrefix("/revealjs/", http.FileServer(&assetfs.AssetFS{
-		Asset:     Asset,
-		AssetDir:  AssetDir,
-		AssetInfo: AssetInfo,
-		Prefix:    "assets/revealjs",
-	})))
+func (server *Server) Serve() {
 	fmt.Printf("Accepting connections at http://0:%d/\n", server.port)
+	http.HandleFunc("/revealjs/", server.HandleStatic)
 	http.ListenAndServe(fmt.Sprintf(":%d", server.port), nil)
 }
 
-func (server *Server) handleExample(w http.ResponseWriter, r *http.Request) {
+func (server *Server) HandleExample(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "revealgo!")
+}
+
+func (server *Server) HandleStatic(w http.ResponseWriter, r *http.Request) {
+	filepath := "assets" + r.URL.Path
+	data, err := Asset(filepath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	mimeType := server.DetectContentType(filepath, data)
+	w.Header().Set("Content-Type", mimeType)
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	if _, err := w.Write(data); err != nil {
+		log.Println("unable to write data.")
+	}
+}
+
+func (server *Server) DetectContentType(path string, data []byte) string {
+	if strings.HasSuffix(path, ".css") {
+		return "text/css"
+	} else if strings.HasSuffix(path, ".js") {
+		return "application/javascript"
+	}
+	return http.DetectContentType(data)
 }
