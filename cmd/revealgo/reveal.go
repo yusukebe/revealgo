f19c7f90cda4783ee172c0b9587dbae380ced115
Yusuke Wada <yusuke@kamawada.com>
2021-10-14 08:17:29 +0900

    Refactor/option parser (#17)
    * Move utility functions to util.go
    * Refactored option parser and add tests for cli.go

--- cmd/revealgo/reveal.go
+++ cmd/revealgo/reveal.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
+
 	"github.com/yusukebe/revealgo"
 )
 
 func main() {
-	cli := revealgo.CLI{}
-	cli.Run()
+	cli := revealgo.CLI{OutStream: os.Stdout, ErrStream: os.Stderr}
+	exitCode := cli.Run(os.Args[1:])
+	os.Exit(exitCode)
 }
