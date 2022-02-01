e9322fa09c6c60b2fc45f65152d9ed0b167d3e80
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-02-01 14:10:08 +0530

    Automatic Browser Open (#27)
    * feat: open browser window
    
    wait for sometime allowing server to run
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * feat: add flag to disable browser opening
    
    check for disable auto open flag before launching go routine
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>
    
    * refactor: log error instead of returning
    
    golint needs to check error if any
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- util.go
+++ util.go
@@ -2,8 +2,31 @@ package revealgo
 
 import (
 	"fmt"
+	"os/exec"
+	"runtime"
 	"strings"
+	"time"
 )
+
+func openBrowser(port int) {
+	<-time.After(100 * time.Millisecond)
+	url := fmt.Sprintf("http://localhost:%d/", port)
+	var args []string
+	var cmd string
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	if err := exec.Command(cmd, args...).Start(); err != nil {
+		fmt.Printf("error when trying to open browser: %s", err.Error())
+	}
+}
 
 func addExtention(path string, ext string) string {
 	if strings.HasSuffix(path, fmt.Sprintf(".%s", ext)) {
